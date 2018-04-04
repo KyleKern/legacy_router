c39752efdb59d69faa1c75de89617b0de67fac93
KyleKern <kyle.z.kern@gmail.com>
2018-04-04 16:18:37 -0700

    Update legacy_router_test.py

--- legacy_router_test.py
+++ legacy_router_test.py
@@ -15,38 +15,40 @@ def myNetwork():
 
     net = Mininet( topo=None,
                    build=False,
-                   ipBase='10.0.0.0/8')
+                   ipBase='10.0.0.0/24')
 
+   ## defaultIP ='192.168.1.1/24'
     info( '*** Adding controller\n' )
     info( '*** Add switches\n')
-    r1 = net.addHost('r1', cls=Node, ip='0.0.0.0')
+    r1 = net.addHost('r1', cls=Node, ip='192.168.1.10/24')
     r1.cmd('sysctl -w net.ipv4.ip_forward=1')
 
     info( '*** Add hosts\n')
-    h2 = net.addHost('h2', cls=Host, ip='127.0.0.2', defaultRoute=None)
-    h1 = net.addHost('h1', cls=Host, ip='127.0.0.1', defaultRoute=None)
+    h2 = net.addHost('h2', cls=Host, ip='192.168.2.100/24', defaultRoute='via 192.168.2.1')
+    h1 = net.addHost('h1', cls=Host, ip='192.168.1.100/24', defaultRoute='via 192.168.1.10')
 
     info( '*** Add links\n')
-    net.addLink(h1, r1)
-    net.addLink(r1, h2)
+
+    net.addLink(h1, r1, intfName2='r1-eth0', params2={'ip':'192.168.1.10/24'})
+    net.addLink(h2, r1, intfName2='r1-eth1', params2={'ip':'192.168.2.1/24'})
 
     info( '*** Starting network\n')
     net.build()
     info( '*** Starting controllers\n')
-    for controller in net.controllers:
 
     info( '*** Adding controller\n' )
     info( '*** Add switches\n')
-    r1 = net.addHost('r1', cls=Node, ip='0.0.0.0')
+    r1 = net.addHost('r1', cls=Node, ip='192.168.1.10/24')
     r1.cmd('sysctl -w net.ipv4.ip_forward=1')
 
     info( '*** Add hosts\n')
-    h2 = net.addHost('h2', cls=Host, ip='10.0.0.2', defaultRoute=None)
-    h1 = net.addHost('h1', cls=Host, ip='10.0.0.1', defaultRoute=None)
+    h2 = net.addHost('h2', cls=Host, ip='192.168.2.100/24', defaultRoute='via 192.168.2.1')
+    h1 = net.addHost('h1', cls=Host, ip='192.168.1.100/24', defaultRoute='via 192.168.1.10')
 
     info( '*** Add links\n')
-    net.addLink(h1, r1)
-    net.addLink(r1, h2)
+
+    net.addLink(h1, r1, intfName2='r1-eth0', params2={'ip':'192.168.1.10/24'})
+    net.addLink(h2, r1, intfName2='r1-eth1', params2={'ip':'192.168.2.1/24'})
 
     info( '*** Starting network\n')
     net.build()
@@ -64,3 +66,5 @@ def myNetwork():
 if __name__ == '__main__':
     setLogLevel( 'info' )
     myNetwork()
+
+
